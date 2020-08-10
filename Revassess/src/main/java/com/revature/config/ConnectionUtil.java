c6fdf57522232fa7cf75896aa72c008f54da26f6
Daniel Kessler <djk172@case.edu>
2020-08-10 16:27:24 -0400

    tier 3 actual push

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -1,6 +1,8 @@
 package com.revature.config;
 
-import java.sql.Connection;
+import org.postgresql.Driver;
+
+import java.sql.*;
 
 /**
  * 
@@ -16,7 +18,7 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "revassess-kessler.cfgzpq9si94d.us-east-1.rds.amazonaws.com";
+	public static final String URL = "jdbc:postgresql://revassess-kessler.cfgzpq9si94d.us-east-1.rds.amazonaws.com:5432/postgres";
 	// add your jdbc username
 	public static final String USERNAME = "daniel_kessler";
 	// add your jdbc password
@@ -26,16 +28,53 @@ public class ConnectionUtil {
 	// name of the created sequence in tier 3
 	public static final String TIER_3_SEQUENCE_NAME = "weirdincrement";
 
+	static {
+		try {
+			DriverManager.registerDriver(new Driver());
+		} catch (SQLException sqlE) {
+			sqlE.printStackTrace();
+		}
+	}
+
 	// implement this method to connect to the db and return the connection object
-	public Connection connect(){
-		return null;
+	public Connection connect() throws SQLException {
+		return DriverManager.getConnection(URL, USERNAME, PASSWORD);
 	}
 
 
 	//implement this method with a callable statement that calls the absolute value sql function
-	public long callAbsoluteValueFunction(long value){
-		return value;
+	public long callAbsoluteValueFunction(long value) throws SQLException {
+		CallableStatement cstmt = null;
+		Connection conn = null;
+		long returnLong = 0;
+
+		try {
+			String SQL = "SELECT Abs(?) AS AbsNum;";
+			conn = connect();
+			cstmt = conn.prepareCall (SQL);
+
+			cstmt.setLong(1, value);
+			ResultSet rs = cstmt.executeQuery();
+
+			while(rs.next()) {
+				try{
+					returnLong = rs.getLong("AbsNum");
+					return returnLong;
+				} catch (SQLException sqlException){
+					sqlException.printStackTrace();
+				}
+			}
+		}
+		catch (SQLException e) {
+			System.out.println(e);
+		}
+		finally {
+			cstmt.close();
+		}
+
+		return returnLong;
 	}
+
 	
 
 	//make the class into a singleton
